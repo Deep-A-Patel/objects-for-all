37d2a330ace512715a68151c9650e71e171f576d
Deep Patel <deep.ag123007@gmail.com>
2019-04-29 15:38:52 -0500

    able to add stuff to json

--- src/scripts/dbcalls.js
+++ src/scripts/dbcalls.js
@@ -1,32 +1,35 @@
 console.log("What's in the junk drawer?");
 
 const API = {
-    getCategories: function () {
-        return fetch("http://localhost:8088/category")
-            .then(response => response.json());
-    },
-    getJunk: function () {
-        return fetch("http://localhost:8088/junk")
-            .then(response => response.json());
-    },
-    getJunkWithCategory: function () {
-        return fetch("http://localhost:8088/category/1?_embed=junk")
-            .then(response => response.json());
-    },
-    getCategoryWithJunk: function () {
-        return fetch("http://localhost:8088/category/1?_embed=junk")
-        .then(response => response.json());
-    },
-    saveCategory: function (obj) {
-        return fetch("http://localhost:8088/category",{
-            method: "POST",
-            headers: {
-            "Content-Type": "application/json"
-            },
-            body: JSON.stringify(obj)
-        })
-        .then(response => response.json())
-    }
+  getCategories: function() {
+    return fetch("http://localhost:8088/category").then(response =>
+      response.json()
+    );
+  },
+  getJunk: function() {
+    return fetch("http://localhost:8088/junk").then(response =>
+      response.json()
+    );
+  },
+  getJunkWithCategory: function() {
+    return fetch("http://localhost:8088/category/1?_embed=junk").then(
+      response => response.json()
+    );
+  },
+  getCategoryWithJunk: function() {
+    return fetch("http://localhost:8088/category/1?_embed=junk").then(
+      response => response.json()
+    );
+  },
+  saveCategory: function(obj) {
+    return fetch("http://localhost:8088/category", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(obj)
+    }).then(response => response.json());
+  }
 };
 
 // foodFactory = (foodItem) => {
@@ -63,22 +66,3 @@ const API = {
 
 // const getDataButton2 = document.getElementById("btn-getData2");
 // getDataButton2.addEventListener("click", () => getData("food"));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
